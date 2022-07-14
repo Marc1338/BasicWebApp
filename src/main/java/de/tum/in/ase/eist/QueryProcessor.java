c87fc33b248c6d739a767d399eb8e240894c5ee8
Marc Roig Kunzmann <ge42yog@mytum.de>
2022-07-14 17:02:59 +0200

    Lets go

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,8 +12,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Marc";
-        } else { // TODO extend the programm here
+            return "Marc";
+
+        } else if (query.contains("what is 1 + 1")){
+            return "2";
+        } else  { // TODO extend the programm here
             return "";
         }
     }
